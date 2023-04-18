1e1c95f3fb316601e7410c6caadf15cc262212fd
Shaqal7 <shaqal7@gmail.com>
2023-04-18 19:50:59 +0200

    ref

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,10 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  /* CSS Reset */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
   html,
   body {
     padding: 0;
     margin: 0;
+    width: 100%;
+    overflow-x: hidden;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
