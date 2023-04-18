3c49d3850eab52e62745221f09cad56ac6a059cc
Shaqal7 <shaqal7@gmail.com>
2023-04-18 23:28:00 +0200

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,10 @@
 // styles/theme.ts
 export const theme = {
-  primaryColor: '#1a1a1a',
-  secondaryColor: '#ffffff',
-  primaryAccent: '#ff8f00',
-  backgroundColor: '#f0f0f0',
-  textColor: '#333',
+  primaryColor: '#1A2A4C', //(dark blue)
+  secondaryColor: '#4F98D9', //(light blue)
+  accentColor: '#F5A623', //(orange)
+  primaryAccent: '#ff8f00', //(orange)
+  backgroundColor: '#f0f0f0', //(light grey)
+  textColor: '#333', //(dark grey)
   // Other theme values here...
 };
