f6aed65e88e8770121c476ab1c5ab0fab16075b4
Shaqal7 <shaqal7@gmail.com>
2023-04-18 22:02:05 +0200

    added hamburger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.nav`
@@ -7,10 +7,6 @@ const HeaderContainer = styled.nav`
   align-items: center;
   padding: 1rem 2rem;
   background-color: ${({ theme }) => theme.primaryColor};
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
 `;
 
 const Logo = styled.div`
@@ -19,14 +15,25 @@ const Logo = styled.div`
   color: ${({ theme }) => theme.secondaryColor};
 `;
 
-const NavLinks = styled.ul`
+const NavLinks = styled.ul<{ open: boolean }>`
   display: flex;
   gap: 1.5rem;
 
   @media (max-width: 768px) {
-    margin-top: 1rem;
+    position: fixed;
+    top: 64px; // Change this value if the header height is different
+    left: 0;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+    padding: 1rem;
+    background-color: ${({ theme }) => theme.primaryColor};
+    transform: ${({ open }) => (open ? 'translateY(0)' : 'translateY(-100%)')};
+    transition: transform 0.3s ease-in-out;
+    z-index: 1000; // Ensures the menu stays on top of other elements
   }
 `;
+
 
 const NavLink = styled.li`
   list-style-type: none;
@@ -34,15 +41,48 @@ const NavLink = styled.li`
   color: ${({ theme }) => theme.secondaryColor};
 `;
 
+const Hamburger = styled.button`
+  display: none;
+  flex-direction: column;
+  gap: 4px;
+  justify-content: space-around;
+  width: 2rem;
+  height: 1.5rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+const HamburgerLine = styled.span`
+  width: 100%;
+  height: 2px;
+  background-color: ${({ theme }) => theme.secondaryColor};
+`;
+
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <HeaderContainer>
       <Logo>VoiceAssistant</Logo>
-      <NavLinks>
-        <NavLink>Features</NavLink>
-        <NavLink>Pricing</NavLink>
-        <NavLink>Support</NavLink>
-        <NavLink>Login/Register</NavLink>
+      <Hamburger onClick={toggleMenu}>
+        <HamburgerLine />
+        <HamburgerLine />
+        <HamburgerLine />
+      </Hamburger>
+      <NavLinks open={menuOpen}>
+        <NavLink onClick={toggleMenu}>Features</NavLink>
+        <NavLink onClick={toggleMenu}>Pricing</NavLink>
+        <NavLink onClick={toggleMenu}>Support</NavLink>
+        <NavLink onClick={toggleMenu}>Login/Register</NavLink>
       </NavLinks>
     </HeaderContainer>
   );
